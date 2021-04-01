4242c9905bc96ea091b1d09b51462242a02e5c44
Marcin Lenkowski <marcin@lenkowski.net>
2021-04-01 09:34:49 +0200

    temporary fix with oauth migration inside API

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RoleTableSeeder::class);
+//        $this->call(RoleTableSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(CategoryTableSeeder::class);
         $this->call(ConfigTableSeeder::class);
