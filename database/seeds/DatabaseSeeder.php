47c07d6e458015295926633e4817fe1d6f866412
Hubert Krzysztofiak <hubertkrzyszt@gmail.com>
2021-05-06 15:00:16 +0200

    Add seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use EscolaLms\Categories\Database\Seeders\CategoriesSeeder;
+use EscolaLms\Courses\Database\Seeders\CoursesSeeder;
+use EscolaLms\Tags\Database\Seeders\TagsSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserTableSeeder::class);
+        $this->call(CategoriesSeeder::class);
+        $this->call(TagsSeeder::class);
+        $this->call(CoursesSeeder::class);
     }
 }
