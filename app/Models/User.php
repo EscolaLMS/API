f0c9af754bcd76f3a3f52ed97dbe6a2279eb4424
Mateusz <mateusz@qunabu.com>
2021-05-08 16:18:46 +0200

    the update of packes & user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,19 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Storage;
-use Laravel\Passport\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles;
 use EscolaLms\Core\Models\User as CoreUser;
 
 // TODO: make user extendable from core + add all traits
 class User extends CoreUser
 {
-    use Notifiable, HasRoles, HasApiTokens;
 }
