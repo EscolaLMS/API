bf75bbe3aee62c02020ad8c1d7c4558e73cef7f4
Mateusz <mateusz@qunabu.com>
2021-08-03 12:42:25 +0200

    images package

--- test.sh
+++ test.sh
@@ -4,3 +4,4 @@ php artisan migrate:fresh --seed
 php artisan passport:keys --force
 php artisan passport:client --personal --no-interaction
 cp storage/oauth-private.key vendor/orchestra/testbench-core/laravel/storage/oauth-private.key
+./vendor/bin/phpunit
