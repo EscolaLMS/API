0678fe5a91520202f0964d67da9713f2a9665f01
Mateusz <mateusz@qunabu.com>
2021-08-25 14:16:03 +0200

    auth seeder update

--- database/seeds/PermissionsSeeder.php
+++ database/seeds/PermissionsSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use EscolaLms\Core\Seeders\RoleTableSeeder;
-use EscolaLms\Auth\Database\Seeders\Auth\AuthPermissionSeeder;
-use EscolaLms\Auth\Database\Seeders\Cart\CartPermissionSeeder;
+use EscolaLms\Auth\Database\Seeders\AuthPermissionSeeder;
+use EscolaLms\Auth\Database\Seeders\CartPermissionSeeder;
 use EscolaLms\Categories\Database\Seeders\CategoriesPermissionSeeder;
 use EscolaLms\Courses\Database\Seeders\CoursesPermissionSeeder;
 use EscolaLms\Payments\Database\Seeders\PaymentsPermissionsSeeder;
