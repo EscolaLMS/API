ab7525e25a8ed176799b8c0cb5c917907f1c0161
Mateusz <mateusz@qunabu.com>
2021-08-25 14:06:46 +0200

    auth seeder update

---
+++ database/seeds/PermissionsSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use EscolaLms\Core\Seeders\RoleTableSeeder;
+use EscolaLms\Auth\Database\Seeders\Auth\AuthPermissionSeeder;
+use EscolaLms\Auth\Database\Seeders\Cart\CartPermissionSeeder;
+use EscolaLms\Categories\Database\Seeders\CategoriesPermissionSeeder;
+use EscolaLms\Courses\Database\Seeders\CoursesPermissionSeeder;
+use EscolaLms\Payments\Database\Seeders\PaymentsPermissionsSeeder;
+use EscolaLms\Files\Database\Seeders\PermissionTableSeeder as FilePermissionTableSeeder;
+use EscolaLms\Pages\Database\Seeders\PermissionTableSeeder as PagesPermissionTableSeeder;
+use EscolaLms\Scorm\Database\Seeders\PermissionTableSeeder as ScormPermissionTableSeeder;
+use Illuminate\Database\Seeder;
+
+class PermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // first populate roles & permissions
+        $this->call(RoleTableSeeder::class);
+        $this->call(AuthPermissionSeeder::class);
+        $this->call(CartPermissionSeeder::class);
+        $this->call(FilePermissionTableSeeder::class);
+        $this->call(CoursesPermissionSeeder::class);
+        $this->call(PaymentsPermissionsSeeder::class);
+        $this->call(CategoriesPermissionSeeder::class);
+        $this->call(PagesPermissionTableSeeder::class);
+        $this->call(ScormPermissionTableSeeder::class);
+    }
+}
