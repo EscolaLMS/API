b695ba04776f9bae1d5665cbee53d1662dcce74f
Mateusz Qunabu <mateusz@qunabu.com>
2021-08-26 17:39:18 +0200

    seeders (#47)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,17 +5,12 @@ namespace Database\Seeders;
 use EscolaLms\Categories\Database\Seeders\CategoriesSeeder;
 use EscolaLms\Categories\Database\Seeders\CategoriesPermissionSeeder;
 
-use EscolaLms\Core\Seeders\RoleTableSeeder;
-use EscolaLms\Courses\Database\Seeders\CoursesPermissionSeeder;
 use EscolaLms\Courses\Database\Seeders\CoursesSeeder;
-use EscolaLms\Payments\Database\Seeders\PaymentsPermissionsSeeder;
 use EscolaLms\Payments\Database\Seeders\PaymentsSeeder;
 use EscolaLms\Tags\Database\Seeders\TagsSeeder;
-use EscolaLms\Files\Database\Seeders\PermissionTableSeeder as FilePermissionTableSeeder;
-
 use EscolaLms\Pages\Database\Seeders\DatabaseSeeder as PagesDatabaseSeeder;
-use EscolaLms\Pages\Database\Seeders\PermissionTableSeeder as PagesPermissionTableSeeder;
-use EscolaLms\Scorm\Database\Seeders\PermissionTableSeeder as ScormPermissionTableSeeder;
+use EscolaLms\Cart\Database\Seeders\OrdersSeeder;
+use EscolaLms\Cart\Database\Seeders\ProgressSeeder;
 
 use Illuminate\Database\Seeder;
 
@@ -38,7 +33,9 @@ class DatabaseSeeder extends Seeder
         $this->call(CategoriesSeeder::class);
         $this->call(TagsSeeder::class);
         $this->call(CoursesSeeder::class);
+
         $this->call(PaymentsSeeder::class);
+
         $this->call(PagesDatabaseSeeder::class);
     }
 }
