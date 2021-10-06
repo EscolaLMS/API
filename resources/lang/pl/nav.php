045afdb5df5508be80816e7fea7fc9f07510b96f
Paweł Chołaj <pawel.cholaj@escola.pl>
2021-10-06 12:18:01 +0200

    Polish language files

---
+++ resources/lang/pl/nav.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Course' => 'Kurs',
+    'Cover' => 'Okładka',
+    'Video' => 'Video',
+    'Curriculum' => 'Program',
+    'Rates' => 'Stawki',
+    'Students' => 'Studenci',
+    'Flashcards' => 'Fiszki'
+];
