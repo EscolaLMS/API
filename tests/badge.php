900c19fe61fc5ceb9df612baf024d8241e26d070
Mateusz <mateusz@qunabu.com>
2021-11-02 19:30:03 +0100

    generate and upload articat cc

---
+++ tests/badge.php
@@ -0,0 +1,9 @@
+<?php
+
+$xml = simplexml_load_file('clover.xml');
+
+$coveredStatements = $xml->project->metrics['coveredstatements'];
+$totalStatements = $xml->project->metrics['statements'];
+$percentage = round(min(1, $coveredStatements / $totalStatements) * 100);
+
+file_put_contents('tests/cc-badge.svg', file_get_contents("https://img.shields.io/badge/coverage-$percentage%25-brightgreen"));
