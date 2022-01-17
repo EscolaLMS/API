ab2cd15bbc6070f86be0620bd4f441b42387af67
Mateusz Wojczal <mateuszwojczal@Mateuszs-MacBook-Pro.local>
2022-01-17 16:59:52 +0100

    badnges fix

--- tests/badge.php
+++ tests/badge.php
@@ -22,10 +22,12 @@ file_put_contents('tests/cc-badge.svg', file_get_contents("https://img.shields.i
 
 echo 'Code coverage is ' . $coverage . '% - OK!' . PHP_EOL;
 
-$file = "coverage.txt";
-$data = file($file);
-$line = ($data[count($data) - 1]);
+$coverage_txt = file_get_contents("coverage.txt");
 
+preg_match('/Tests: (\d+)/', $coverage_txt, $output_array1);
+preg_match('/Assertions: (\d+)/', $coverage_txt, $output_array2);
+
+$tests = "Tests: " . $output_array1[1] . ", Assertions: " . $output_array2[1];
 
 // https://img.shields.io/badge/Tests-OK%20(4%20tests%2C%2016%20assertions)-blue
-file_put_contents('tests/cc-tests.svg', file_get_contents("https://img.shields.io/badge/$line-blue"));
+file_put_contents('tests/cc-tests.svg', file_get_contents("https://img.shields.io/badge/$tests-blue"));
