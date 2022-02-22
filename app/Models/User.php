e1349148920e8bccf8096821b1cdc24102716623
krz <krzysztof.dziedziech@escola.pl>
2022-02-22 15:25:17 +0100

    add questionnaire

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,14 +4,14 @@ namespace App\Models;
 
 //use EscolaLms\Core\Models\User as CoreUser;
 use EscolaLms\Auth\Models\User as CoreUser;
-use EscolaLms\Cart\Models\Contracts\CanOrder as ContractsCanOrder;
+//use EscolaLms\Cart\Models\Contracts\CanOrder as ContractsCanOrder;
 use EscolaLms\Cart\Models\Traits\CanOrder;
 use EscolaLms\Courses\Models\Traits\HasCourses;
 use EscolaLms\Payments\Concerns\Billable;
 use EscolaLms\Payments\Contracts\Billable as ContractsBillable;
 
 // TODO: make user extendable from core + add all traits
-class User extends CoreUser implements ContractsBillable, ContractsCanOrder
+class User extends CoreUser implements ContractsBillable//, ContractsCanOrder
 {
     use Billable;
     use CanOrder;
---
+++ database/migrations/2022_02_22_151613_add_records_to_table_questionnaire_model_type.php
@@ -0,0 +1,28 @@
+<?php
+
+use EscolaLms\Questionnaire\Models\QuestionnaireModelType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRecordsToTableQuestionnaireModelType extends Migration
+{
+    public function up(): void
+    {
+        if (!QuestionnaireModelType::query()->find('course', 'title')) {
+            $questionnaireModelType = new QuestionnaireModelType([
+                'title' => 'Course',
+                'model_class' => 'EscolaLms\Courses\Models\Course',
+            ]);
+            $questionnaireModelType->save();
+        }
+    }
+
+    public function down(): void
+    {
+        $questionnaireModelType = QuestionnaireModelType::query()->find('course', 'title');
+        if ($questionnaireModelType) {
+            $questionnaireModelType->delete();
+        }
+    }
+}
