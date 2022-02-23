cadd7f3d8b1be5b38d2216124cf7e4e79a59d5ed
KrzysztofDziedziechEscolasoft <96292232+KrzysztofDziedziechEscolasoft@users.noreply.github.com>
2022-02-23 12:05:44 +0100

    Feature/star 440 (#137)
    * release (#126)
    
    * Commit from GitHub Actions (Tests Code Coverage)
    
    * Update DatabaseSeeder.php
    
    * Commit from GitHub Actions (Tests Code Coverage)
    
    * Update DatabaseSeeder.php
    
    * Commit from GitHub Actions (Tests Code Coverage)
    
    * minor updates (#121)
    
    * minor updates
    
    * minor updates
    
    * minor updates
    
    * Update composer.json
    
    * Delete AppBaseController.php
    
    * change swagger version to 8.1.0
    
    Co-authored-by: Mateusz Wojczal <mateusz@wojczal.com>
    Co-authored-by: Tomasz Smolarek <tomasz.smolarek@escolasoft.com>
    
    * Commit from GitHub Actions (Tests Code Coverage)
    
    Co-authored-by: qunabu <qunabu@users.noreply.github.com>
    Co-authored-by: Hubert Krzysztofiak <62691459+HerbertIV@users.noreply.github.com>
    Co-authored-by: HerbertIV <HerbertIV@users.noreply.github.com>
    Co-authored-by: Mateusz Wojczal <mateusz@wojczal.com>
    Co-authored-by: Tomasz Smolarek <tomasz.smolarek@escolasoft.com>
    
    * add questionnaire
    
    * reroll User
    
    * fix
    
    Co-authored-by: Mateusz Qunabu <mateusz@qunabu.com>
    Co-authored-by: qunabu <qunabu@users.noreply.github.com>
    Co-authored-by: Hubert Krzysztofiak <62691459+HerbertIV@users.noreply.github.com>
    Co-authored-by: HerbertIV <HerbertIV@users.noreply.github.com>
    Co-authored-by: Mateusz Wojczal <mateusz@wojczal.com>
    Co-authored-by: Tomasz Smolarek <tomasz.smolarek@escolasoft.com>
    Co-authored-by: krz <krzysztof.dziedziech@escola.pl>

---
+++ database/migrations/2022_02_22_151613_add_records_to_table_questionnaire_model_type.php
@@ -0,0 +1,28 @@
+<?php
+
+use EscolaLms\Questionnaire\Models\QuestionnaireModelType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRecordsToTableQuestionnaireModelType extends Migration
+{
+    public function up(): void
+    {
+        if (!QuestionnaireModelType::query()->where('title', '=', 'course')->first()) {
+            $questionnaireModelType = new QuestionnaireModelType([
+                'title' => 'Course',
+                'model_class' => 'EscolaLms\Courses\Models\Course',
+            ]);
+            $questionnaireModelType->save();
+        }
+    }
+
+    public function down(): void
+    {
+        $questionnaireModelType = QuestionnaireModelType::query()->where('title', '=', 'course')->first();
+        if ($questionnaireModelType) {
+            $questionnaireModelType->delete();
+        }
+    }
+}
