572b16b1aa72280f9724c841979a1974f7326a15
Paweł Chołaj <pawel.cholaj@escolasoft.com>
2022-03-25 10:35:01 +0100

    Release (#161)
    * Add Escolalms/Vouchers to API
    
    * VoucherPermissionsSeeder
    
    * Add php 8.1 test
    
    * Run vouchers tests
    
    * Service providers cleanup
    
    * Docs
    
    * Service provider update
    
    * Trying to fix shop registration
    
    * ...?
    
    * Override testbench testcase
    
    * Probably working version
    
    * Forgot `use` statement
    
    * Typo
    
    * Commit from GitHub Actions (Tests Code Coverage)
    
    * Dispatch event for user attached or detached from webinar
    
    * Dispatch event for user attached or detached from course
    
    * Commit from GitHub Actions (Tests Code Coverage)
    
    * - Updated Payments config file (#160)
    
    - Updated Productable models to include latest Cart changes
    
    * Commit from GitHub Actions (Tests Code Coverage)
    
    Co-authored-by: Maciej Rymarz <maciej.rymarz@escolasoft.com>
    Co-authored-by: Mateusz Qunabu <mateusz@qunabu.com>
    Co-authored-by: pa-cholek <pa-cholek@users.noreply.github.com>
    Co-authored-by: qunabu <qunabu@users.noreply.github.com>

--- config/payments.php
+++ config/payments.php
@@ -7,28 +7,24 @@ return [
     'default_gateway' => env('PAYMENTS_DEFAULT_GATEWAY', 'Stripe'),
     'default_currency' => env('PAYMENTS_DEFAULT_CURRENCY', EscolaLms\Payments\Enums\Currency::USD),
 
-    /**
-     * Eloquent models settings
-     */
-
-    'payment_model' => env('PAYMENTS_PAYMENT_MODEL', EscolaLms\Payments\Models\Payment::class),
-    'fallback_billable_model' => env('PAYMENTS_BILLABLE_MODEL', App\Models\User::class),
-
-    /**
-     * Urls settings
-     */
-
-    'url_redirect' => env('PAYMENTS_REDIRECT_URL', '/'),
-    'url_notification' => null,
-
     /** 
      * Driver specific settings 
      */
     'drivers' => [
         'free' => [],
         'stripe' => [
-            'key' => env('PAYMENTS_STRIPE_SECRET_KEY', 'sk_test_51Ig8icJ9tg9t712TG1Odn17fisxXM9y01YrDBxC4vd6FJMUsbB3bQvXYs8Oiz9U2GLH1mxwQ2BCjXcjc3gxEPKTT00tx6wtVco'),
-            'publishable_key' => env('PAYMENTS_STRIPE_PUBLISHABLE_KEY', 'pk_test_51Ig8icJ9tg9t712TnCR6sKY9OXwWoFGWH4ERZXoxUVIemnZR0B6Ei0MzjjeuWgOzLYKjPNbT8NbG1ku1T2pGCP4B00GnY0uusI'),
-        ]
+            'enabled' => true,
+            'secret_key' => env('PAYMENTS_STRIPE_SECRET_KEY'),
+            'publishable_key' => env('PAYMENTS_STRIPE_PUBLISHABLE_KEY'),
+            'allowed_payment_method_types' => ['card'],
+        ],
+        'przelewy24' => [
+            'enabled' => true,
+            'live' => env('PAYMENTS_PRZELEWY24_LIVE', false),
+            'merchant_id' => env('PAYMENTS_PRZELEWY24_MERCHANT_ID'),
+            'pos_id' => env('PAYMENTS_PRZELEWY24_POS_ID'),
+            'api_key' => env('PAYMENTS_PRZELEWY24_API_KEY'),
+            'crc' => env('PAYMENTS_PRZELEWY24_CRC'),
+        ],
     ]
 ];
