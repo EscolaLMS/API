3fe7693d01e51a532600c304687f29b3edffc675
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-03-31 14:34:48 +0200

    Repair seeder

--- database/seeds/ConsultationsSeeder.php
+++ database/seeds/ConsultationsSeeder.php
@@ -5,9 +5,11 @@ namespace Database\Seeders;
 use EscolaLms\Auth\Models\User;
 use EscolaLms\Cart\Models\Order;
 use EscolaLms\Cart\Models\OrderItem;
+use EscolaLms\Consultations\Enum\ConsultationTermStatusEnum;
 use EscolaLms\Consultations\Models\Consultation;
 use EscolaLms\Consultations\Models\ConsultationProposedTerm;
 use EscolaLms\Consultations\Models\ConsultationTerm;
+use EscolaLms\Consultations\Models\ConsultationUserPivot;
 use Illuminate\Database\Seeder;
 
 class ConsultationsSeeder extends Seeder
@@ -19,40 +21,25 @@ class ConsultationsSeeder extends Seeder
      */
     public function run()
     {
+        $statuses = collect([
+            ConsultationTermStatusEnum::REPORTED,
+            ConsultationTermStatusEnum::APPROVED,
+            ConsultationTermStatusEnum::REJECT,
+        ]);
+        $modifierDate = collect(['hours', 'days', 'minutes']);
         $users = User::factory(5)->create();
         Consultation::factory(10)
             ->has(ConsultationProposedTerm::factory(3), 'proposedTerms')
             ->create()
-            ->each(function (Consultation $consultation) use($users) {
-                $consultationsForOrder = collect();
-                $consultationsForOrder->push($consultation);
-                $price = $consultationsForOrder->reduce(fn ($acc, Consultation $consultation) => $acc + $consultation->getBuyablePrice(), 0);
-                $this->order = Order::factory()->afterCreating(
-                    function (Order $order) use($consultationsForOrder, $users) {
-                        $items = $order->items()->saveMany(
-                            $consultationsForOrder->map(
-                                function (Consultation $consultation) {
-                                    return OrderItem::query()->make([
-                                        'quantity' => 1,
-                                        'buyable_id' => $consultation->getKey(),
-                                        'buyable_type' => Consultation::class,
-                                    ]);
-                                }
-                            )
-                        );
-                        $items->each(fn (OrderItem $orderItem) =>
-                        ConsultationTerm::factory([
-                            'executed_at' => date('Y-m-d H:i:s', random_int(1640995200, 1648598400)),
-                            'order_item_id' => $orderItem->getKey(),
-                            'user_id' => $users->random()->getKey()
-                        ])->create()
-                        );
-                    }
-                )->create([
-                    'user_id' => $users->random()->getKey(),
-                    'total' => $price,
-                    'subtotal' => $price,
-                ]);
-            });
+            ->each(fn (Consultation $consultation) =>
+                ConsultationUserPivot::factory(3, [
+                    'consultation_id' => $consultation->getKey(),
+                    'user_id' => $users->random(1)->first(),
+                    'executed_status' => $statuses->random(1)->first(),
+                    'executed_at' => now()
+                        ->modify('+' . random_int(1, 10) . ' ' . $modifierDate->random(1)->first())
+                        ->format('Y-m-d H:i:s'),
+                ])->create()
+            );
     }
 }
