2696412bbcab92fba443736c0200fc61e145f1cf
Mateusz Qunabu <mateusz@qunabu.com>
2022-09-28 20:55:39 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
+
+if [ "$DISBALE_PHP_FPM" == 'true' ]
+then
+    rm -f /etc/supervisor/conf.d/php-fpm.conf
+    echo php-fpm.conf disabled
+fi
+
+if [ "$DISBALE_HORIZON" == 'true' ]
+then
+    rm -f /etc/supervisor/custom.d/horizon.conf
+    echo horizon.conf disabled
+fi
+
+if [ "$DISBALE_NGINX" == 'true' ]
+then
+    rm -f /etc/supervisor/custom.d/nginx.conf
+    echo nginx.conf disabled
+fi
+
+if [ "$DISBALE_SCHEDULER" == 'true' ]
+then
+    rm -f /etc/supervisor/custom.d/scheduler.conf
+    echo scheduler.conf disabled
+fi
+
 php docker/envs/envs.php
 php artisan key:generate --force --no-interaction
-php artisan passport:keys --force --no-interaction
+php artisan passport:keys --force --no-interaction 
 php artisan migrate --force
 php artisan passport:client --personal --no-interaction
 php artisan db:seed --class=PermissionsSeeder --force --no-interaction
