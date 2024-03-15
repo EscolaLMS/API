049000d3c717c125d59c043dd9a952a4265803e3
Tomasz Smolarek <59400506+dyfero@users.noreply.github.com>
2024-03-15 13:22:52 +0100

    Update Course.php

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -19,7 +19,10 @@ class Course extends \EscolaLms\Courses\Models\Course implements Productable
 
     public function attachToUser(User $user, int $quantity = 1, ?Product $product = null): void
     {
-        $this->users()->syncWithoutDetaching($user->getKey());
+        $productUser = $product?->users()->where('user_id', $user->getKey())->first()?->pivot;
+
+        $this->users()->syncWithoutDetaching([$user->getKey() => ['end_date' => $productUser?->end_date]]);
+        
         event(new CourseAssigned($user, $this));
         event(new CourseAccessStarted($user, $this));
     }
